139a147d8afad8d3f26502c54e4ac061d7cc34e8
c <chuckiewaitt@gmail.com>
2024-05-02 01:11:32 -0400

    restructure, continuous read

--- wss-test.go
+++ wss-test.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/wsjson"
 )
 
 const AevoHttp string = "https://api.aevo.xyz"
@@ -45,24 +44,10 @@ type Market struct {
 	Greeks           Greeks  `json:"greeks"`
 }
 
-type WssOrderbookData struct {
-	Type           string      `json:"type"`
-	InstrumentId   int         `json:"instrument_id,string"`
-	InstrumentName string      `json:"instrument_name"`
-	InstrumentType string      `json:"instrument_type"`
-	Bids           [][]float64 `json:"bids"` //probably wont work
-	Asks           [][]float64 `json:"asks"`
-	LastUpdated    int         `json:"last_updated,string"`
-	Checksum       int         `json:"checksum,string"`
-}
+var Orderbooks map[string]interface{} = make(map[string]interface{})
 
-type WssOrderbook struct {
-	Channel string `json:"channel"`
-	Data    string `json:"data"`
-}
-
-func markets() []Market {
-	url := AevoHttp + "/markets"
+func markets(asset string) []Market {
+	url := AevoHttp + "/markets?asset=" + asset + "&instrument_type=OPTION"
 
 	req, _ := http.NewRequest("GET", url, nil) //NewRequest + Client.Do used to pass headers, otherwise http.Get can be used
 
@@ -70,7 +55,7 @@ func markets() []Market {
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("markets request error: %v", err)
 	}
 
 	defer res.Body.Close() //Client.Do, http.Get, http.Post, etc all need response Body to be closed when done reading from it
@@ -81,7 +66,7 @@ func markets() []Market {
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&markets)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("markets json decode error: %v", err)
 	}
 
 	return markets
@@ -114,45 +99,103 @@ func orderbookJson(instruments []string) []byte {
 
 	jsonData, err := json.Marshal(data)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("orderbook json marshal error: %v", err)
 	}
 
 	return jsonData
 }
 
+func wssReqOrderbook(instruments []string, ctx context.Context, c *websocket.Conn) {
+	var data []byte
+	for i := 0; true; i += 20 {
+		if i+20 < len(instruments) {
+			data = orderbookJson(instruments[i : i+20])
+		} else {
+			data = orderbookJson(instruments[i:])
+		}
+
+		fmt.Printf("%v\n\n", string(data))
+		err := c.Write(ctx, 1, data)
+		if err != nil {
+			log.Fatalf("Write error: %v", err)
+		}
+
+		if i+20 > len(instruments) {
+			break
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+// func unmarshalRes(raw []byte, res *map[string]interface{}) {
+// 	err := json.Unmarshal(raw, &res)
+// 	if err != nil {
+// 		log.Fatalf("Error decoding: %s", err)
+// 	}
+// }
+
+func wssRead(ctx context.Context, c *websocket.Conn) []byte {
+	_, raw, err := c.Read(ctx)
+	if err != nil {
+		log.Fatalf("Read error: %v", err)
+	}
+
+	return raw
+}
+
+func constructOrderbooks(instruments []string) {
+	for _, instrument := range instruments {
+		Orderbooks[instrument] = make(map[string]interface{})
+	}
+}
+
+// func updateOrderbooks(res map[string]interface{}) {
+// 	key := res["channel"]["orderbook"]
+// 	Orderbooks[key] = res
+// }
+
 func main() {
-	markets := markets()
+	markets := markets("ETH")
 	instruments := instruments(markets)
+	fmt.Printf("Number of instruments: %v\n\n", len(instruments))
+	constructOrderbooks(instruments)
+	// fmt.Printf("%+v", Orderbooks)
 	// fmt.Printf("%v\n", instruments)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*20)
 	defer cancel()
 
 	c, res, err := websocket.Dial(ctx, AevoWss, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Dial error: %v", err)
 	}
-	fmt.Printf("%v\n", res)
+	fmt.Printf("%v\n\n", res)
+	defer c.Close(websocket.StatusNormalClosure, "")
 	defer c.CloseNow()
 
-	data := orderbookJson(instruments[1:5])
-	err = wsjson.Write(ctx, c, data)
-	if err != nil {
-		log.Fatal(err)
-	}
+	wssReqOrderbook(instruments, ctx, c)
 
-	_, raw, err := c.Read(ctx)
-	if err != nil {
-		log.Fatal(err)
+	var orderbookRaw []byte
+	// var orderbookRes map[string]interface{}
+	for {
+		orderbookRaw = wssRead(ctx, c)
+		// go unmarshalRes(orderbookRaw, &orderbookRes)
+		go fmt.Printf("Decoded JSON response:\n %+v\n", string(orderbookRaw))
+		// go fmt.Printf("%+v\n", reflect.TypeOf(orderbookRes["channel"]["orderbook"]))
 	}
-	fmt.Printf("%v\n", string(raw))
-
-	var orderbookRes WssOrderbook
-	err = json.Unmarshal(raw, &orderbookRes)
-	if err != nil {
-		log.Fatalf("Error unmarshaling: %s", err)
-	}
-	fmt.Printf("%+v\n", orderbookRes)
-
-	c.Close(websocket.StatusNormalClosure, "")
 }
+
+// _, r, err := c.Reader(ctx)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// fmt.Printf("%+v\n\n", r)
+
+// var orderbookRes map[string]interface{}
+// decoder := json.NewDecoder(r)
+// err = decoder.Decode(&orderbookRes)
+// if err != nil {
+// 	log.Fatalf("Error decoding: %s", err)
+// }
+// fmt.Printf("Decoded JSON response:\n %+v\n", orderbookRes)
