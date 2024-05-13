b8c821c095b397195e74199a2d64e6154f74742b
c <chuckiewaitt@gmail.com>
2024-05-13 04:06:58 -0400

    APY, sort by APY

--- wss-test.go
+++ wss-test.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"net/http"
 	"reflect"
+	"sort"
 	"strconv"
 	"strings"
 	"text/template"
@@ -68,6 +69,9 @@ type OrderbookData struct {
 }
 
 type ArbTable struct {
+	Asset     string
+	Expiry    string
+	Strike    float64
 	Bids      []Order
 	Asks      []Order
 	BidType   string
@@ -299,11 +303,26 @@ func updateIndex(res map[string]interface{}) {
 	fmt.Printf("index: %+v\n\n", Index)
 }
 
-func updateArbTables() {
-	index := Index["ETH"]
+func findApy(expiry string, relProfit float64) float64 {
+	ts, err := time.Parse("02Jan06", expiry)
+	if err != nil {
+		log.Printf("findApy: error parsing expiry to timestamp: %v\n\n", err)
+		return 0.0
+	}
+	timestamp := float64(ts.Unix())
+	now := float64(time.Now().Unix())
+
+	apy := (365 / math.Ceil((1+timestamp-now)/86400)) * relProfit
+
+	return apy
+}
+
+func updateArbTables(asset string) {
+	index := Index[asset]
 
 	for key, orderbook := range Orderbooks {
 		components := strings.Split(key, "-")
+		expiry := components[1]
 		strike, err := strconv.ParseFloat(components[2], 64)
 		if err != nil {
 			fmt.Printf("updateArbTables: unable to convert strike string to float64: %v\n", err)
@@ -338,15 +357,19 @@ func updateArbTables() {
 			putAsk = orderbook2.Asks[0].Price
 			absProfit = math.Abs((index + putAsk) - (strike + callBid))
 			relProfit := absProfit / (index + putAsk + callBid) * 100
+			apy := findApy(expiry, relProfit)
 
 			ArbTables[keyTrim] = &ArbTable{
+				Asset:     asset,
+				Expiry:    expiry,
+				Strike:    strike,
 				Bids:      orderbook.Bids,
 				Asks:      orderbook2.Asks,
 				BidType:   "C",
 				AskType:   "P",
 				AbsProfit: absProfit,
 				RelProfit: relProfit,
-				Apy:       0.0, //implement later, use time.Parse
+				Apy:       apy,
 			}
 		}
 
@@ -357,16 +380,20 @@ func updateArbTables() {
 			putBid = orderbook2.Bids[0].Price
 			thisProfit := math.Abs((index + putBid) - (strike + callAsk))
 			relProfit := thisProfit / (index + callAsk + putBid) * 100
+			apy := findApy(expiry, relProfit)
 
 			if thisProfit > absProfit {
 				ArbTables[keyTrim] = &ArbTable{
+					Asset:     asset,
+					Expiry:    expiry,
+					Strike:    strike,
 					Bids:      orderbook2.Bids,
 					Asks:      orderbook.Asks,
 					BidType:   "P",
 					AskType:   "C",
 					AbsProfit: thisProfit,
 					RelProfit: relProfit,
-					Apy:       0.0, //implement later, use time.Parse
+					Apy:       apy,
 				}
 			}
 		}
@@ -409,7 +436,7 @@ func wssReadLoop(ctx context.Context, c *websocket.Conn) { //add exit condition,
 
 		if strings.Contains(channel, "index") {
 			updateIndex(res)
-			updateArbTables()
+			updateArbTables("ETH")
 		}
 
 		// duration := time.Since(start)
@@ -450,14 +477,24 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func arbTableHandler(w http.ResponseWriter, r *http.Request) {
+	arbTablesSlice := make([]*ArbTable, len(ArbTables))
+	i := 0
+	for _, table := range ArbTables {
+		arbTablesSlice[i] = table
+		i++
+	}
+	// sort.Slice(arbTablesSlice, func(i, j int) bool { return arbTablesSlice[i].RelProfit > arbTablesSlice[j].RelProfit })
+	sort.Slice(arbTablesSlice, func(i, j int) bool { return arbTablesSlice[i].Apy > arbTablesSlice[j].Apy })
+
 	responseStr := ""
-	for key, value := range ArbTables {
-		responseStr += fmt.Sprintf(`<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>`,
-			key,
+	for _, value := range arbTablesSlice {
+		responseStr += fmt.Sprintf(`<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>`,
+			value.Expiry,
+			strconv.FormatFloat(value.Strike, 'f', 3, 64),
 			value.BidType,
-			strconv.FormatFloat(value.Bids[0].Amount, 'f', 3, 64),
+			strconv.FormatFloat(value.Bids[0].Price, 'f', 3, 64),
 			value.AskType,
-			strconv.FormatFloat(value.Asks[0].Amount, 'f', 3, 64),
+			strconv.FormatFloat(value.Asks[0].Price, 'f', 3, 64),
 			strconv.FormatFloat(value.AbsProfit, 'f', 3, 64),
 			strconv.FormatFloat(value.RelProfit, 'f', 3, 64),
 			strconv.FormatFloat(value.Apy, 'f', 3, 64),
